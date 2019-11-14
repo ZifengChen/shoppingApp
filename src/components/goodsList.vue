4ee65d7d544408bef82b2e03718095317a84627d
ZifengChen <ken36chan@gmail.com>
2019-11-14 11:11:50 +0800

    updated

---
+++ src/components/goodsList.vue
@@ -0,0 +1,72 @@
+<template>
+    <div name="show">
+        <ul class="goodList">
+            <li v-bind:key="good.goodName" v-for="good in list">
+                <img v-bind:src="good.img">
+                <p>{{good.goodName}}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "show",
+    data(){
+        var obj = this;
+        var url = "";
+        if(obj.goodId==1){
+            url = "json/sb.json";
+        }
+        else if(obj.goodId==2){
+            url = "json/shouji.json";
+        }
+        else{
+            url = "json/sb.json";
+        }
+        this.$http.get(url).then(function(res){
+            obj.list=res.data;
+        })
+        return{
+            list:[]
+        }
+    },
+    props:{
+        goodId: Number
+    },
+    watch:{
+        goodId(){
+            var obj = this;
+            var url = "";
+            if(obj.goodId==1){
+                url = "json/sb.json";
+            }
+            else if(obj.goodId==2){
+                url = "json/shouji.json";
+            }
+            this.$http.get(url).then(function(res){
+            obj.list=res.data;
+        })
+        return{
+            list:[]
+        }
+        }
+    }
+}
+</script>
+
+<style>
+    .goodList li{
+        width: 200px;
+        height: 200px;
+        list-style: none;
+        float: left;
+        font-size: 13px;
+        color:red;
+        margin-bottom: 30px;
+    }
+    .goodList img{
+        width: 180px;
+        height: 180px;
+    }
+</style>
---
+++ src/views/First.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="main">
+        <div class="left">
+            <Left/>
+        </div>
+        <div class="right">
+            <div class="top">
+                <img src="../../public/img/title.jpeg" alt="title">
+            </div>
+            <div class="bottom">
+                <Right/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Left from '../components/left';
+import Right from '../components/Right';
+export default {
+    components: {
+        Left,
+        Right
+    }
+}
+</script>
+
+<style>
+    *{
+        padding: 0;
+        margin: 0;
+    }
+    .left{
+        width: 100px;
+        float: left;
+        margin-right: 10px;
+    }
+    .right{
+        width: 1000px;
+        float: left;
+        margin-right: 10px;
+    }
+    .main{
+        width: 1200px;
+        margin: 20px auto;
+    }
+    .top img{
+        width:1000px;
+        height: 200px;
+    }
+    .left,.right{
+        background-color: #f5f5f5;
+        height: 500px;
+    }
+</style>
